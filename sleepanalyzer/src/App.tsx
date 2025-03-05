c1618c05cc0c69d7238de988b3c9237febf5671d
Alicia Yang <akthyang@gmail.com>
2025-03-05 02:18:48 -0500

    made navbar clickable

--- sleepanalyzer/src/App.tsx
+++ sleepanalyzer/src/App.tsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import './App.css'
 
 import Navbar from '@/components/navbar'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Analyze from '@/pages/analyze'
+import Login from '@/pages/login'
+import Mental from '@/pages/mental'
+import Why from '@/pages/why'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+            <Route path="Why" element={<Why />} />
+            <Route path="Analyze" element={<Analyze />} />
+            <Route path="Mental" element={<Mental />} />
+            <Route path="Login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
