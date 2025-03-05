1eff409843143198191e8cb692bbc1b19c7e5fd1
Alicia Yang <akthyang@gmail.com>
2025-03-05 02:19:11 -0500

    added new pages for the website

---
+++ sleepanalyzer/src/pages/Analyze.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const Analyze = (props: Props) => {
+  
+  return (
+    <div>Analyze</div>
+  );
+
+};
+
+export default Analyze;
---
+++ sleepanalyzer/src/pages/Mental.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const Mental = (props: Props) => {
+  
+  return (
+    <div>Mental Health</div>
+  );
+
+};
+
+export default Mental;
