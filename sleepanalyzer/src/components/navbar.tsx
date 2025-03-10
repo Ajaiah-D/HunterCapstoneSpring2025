aa7206911de0b5f0d147da279eb75c647ac1b65b
Alicia Yang <akthyang@gmail.com>
2025-03-10 13:45:52 -0400

    centered links when on hamburger menu

--- sleepanalyzer/src/components/navbar.tsx
+++ sleepanalyzer/src/components/navbar.tsx
@@ -85,7 +85,7 @@ const Navbar = ({ isTopOfPage }: Props) => {
                       </button>
                     </div>
 
-                    <div className='ml-[33%] flex flex-col gap-10 text-2xl'>
+                    <div className='flex flex-col items-center gap-10 text-2xl'>
                       <Link to="/"> Home </Link>
                       <Link to="/why">Why Sleep Analyzer?</Link>
                       <Link to="/analyze">Analyze</Link>
