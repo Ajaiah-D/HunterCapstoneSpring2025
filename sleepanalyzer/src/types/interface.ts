9ec1725ab03e25b3d2edb418764b73e25a3cdd9f
Alicia Yang <akthyang@gmail.com>
2025-03-23 22:31:01 -0400

    added interface used across all pages

---
+++ sleepanalyzer/src/types/interface.ts
@@ -0,0 +1,12 @@
+
+export interface UserLogin {
+    email: string;
+    password: string;
+}
+
+export interface UserSignup {
+    email: string;
+    password: string;
+    confirmPassword: string;
+    displayName: string;
+}
