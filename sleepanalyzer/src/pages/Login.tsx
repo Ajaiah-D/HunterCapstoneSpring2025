7b6bbe0f0a5890be78123fca34c01e694cf05a5c
Alicia Yang <akthyang@gmail.com>
2025-03-23 22:30:35 -0400

    trying to implement sign in and register, only google is working, need to debug email and pwd

--- sleepanalyzer/src/pages/Login.tsx
+++ sleepanalyzer/src/pages/Login.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { UserCircleIcon, LockClosedIcon, EnvelopeIcon } from '@heroicons/react/24/outline';
+import React, { useContext, useState } from 'react';
+import { LockClosedIcon, EnvelopeIcon } from '@heroicons/react/24/outline';
+import { RiGoogleLine } from "react-icons/ri";
 
 import useMediaQuery from '@/hooks/useMediaQuery';
 import CustomButtom from '@/components/CustomButtom';
 import CustomInput from '@/components/CustomInput';
+import { AuthContext } from '@/components/AuthProvider';
+import { useNavigate } from 'react-router';
 
 type Props = {};
 
@@ -12,6 +15,30 @@ const Login = (props: Props) => {
   /* change flex direction of section if screen size is too small */
   const aboveMediumScreen = useMediaQuery("(min-width: 1060px)");
   const flexDirection = aboveMediumScreen ? "flex-row" : "flex-col"
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const {googleSignIn, logIn} = useContext(AuthContext);
+   const navigate = useNavigate();
+
+  const handleGoogleSignin = async(e: React.MouseEvent<HTMLElement>) => {
+      e.preventDefault();
+      try {
+          setLoading(true);
+          console.log("Let's start!")
+          await googleSignIn();
+          navigate("/profile");
+      } catch(error) {
+          if (typeof error === "object" && 
+              error && "message" in error &&
+              typeof error.message === "string"
+          ) {
+            // message gets narrowed to string!
+            setError(error.message)
+            console.log(error);
+          };
+      }
+  }
   
   return (
     <section className={`h-screen
@@ -58,15 +85,15 @@ const Login = (props: Props) => {
             <CustomInput  type="email" 
                           placeholder="Email" 
                           title="Email" 
-                          customization=""/>
-            <EnvelopeIcon className="w-6 text-white absolute right-25 top-1/3 translate-y-5 translate-3" />
+                          />
+            <EnvelopeIcon className="w-6 text-white absolute right-25 top-1/4 translate-y-9 translate-3" />
           </div>
           <div className="w-full">
             <CustomInput  type="password" 
                           placeholder="Password" 
                           title="password" 
-                          customization=""/>
-            <LockClosedIcon className=" w-6 text-white absolute right-22 bottom-1/2 translate-y-11"/>
+                          />
+            <LockClosedIcon className=" w-6 text-white absolute right-22 bottom-1/2"/>
           </div>
           <CustomButtom page="login" 
                         customization="w-5/6">
@@ -76,6 +103,18 @@ const Login = (props: Props) => {
                         customization="w-5/6">
             Sign Up
           </CustomButtom>
+          <div className="gap-5 flex flex-col">
+              <div>
+                <hr className="border-t-2 border-white"/>
+                <p> OR </p>
+                <hr className="border-t-2 border-white"/>
+              </div>
+              <div className="social-icons">
+                <button className="border-2 border-white rounded-xl p-2" onClick={handleGoogleSignin} disabled={loading}>
+                  <RiGoogleLine/>
+                </button>
+              </div>
+          </div>
         </form>
       </div>
     </section>
