da04fcff6710721edd1643bb9867b597caddc85a
Alicia Yang <akthyang@gmail.com>
2025-03-28 17:51:10 -0400

    making index even

--- sleepanalyzer/src/components/ParallaxScroll.tsx
+++ sleepanalyzer/src/components/ParallaxScroll.tsx
@@ -8,46 +8,46 @@ type Props = {
 };
 
 const ParallaxScroll = ({backimage, foreimage, children}:Props) => {
-  const background: BannerLayer = {
-    image: backimage,
-    translateY: [0, 50],
-    opacity: [1, 0.3],
-    scale: [1.05, 1, "easeOutCubic"],
-    shouldAlwaysCompleteAnimation: true
-  };
+    const background: BannerLayer = {
+        image: backimage,
+        translateY: [0, 50],
+        opacity: [1, 0.3],
+        scale: [1.05, 1, "easeOutCubic"],
+        shouldAlwaysCompleteAnimation: true
+    };
 
-  const headline: BannerLayer = {
-    translateY: [0, 30],
-    scale: [1, 1.05, "easeOutCubic"],
-    shouldAlwaysCompleteAnimation: true,
-    expanded: false,
-    children: (
-      <div className="absolute inset-0 flex justify-center items-center">
-        {children}
-      </div>
-    )
-  };
+    const headline: BannerLayer = {
+        translateY: [0, 30],
+        scale: [1, 1.05, "easeOutCubic"],
+        shouldAlwaysCompleteAnimation: true,
+        expanded: false,
+        children: (
+        <div className="absolute inset-0 flex justify-center items-center">
+            {children}
+        </div>
+        )
+    };
 
-  const foreground: BannerLayer = {
-    image: foreimage,
-    translateY: [0, 15],
-    scale: [1, 1.1, "easeOutCubic"],
-    shouldAlwaysCompleteAnimation: true
-  };
+    const foreground: BannerLayer = {
+        image: foreimage,
+        translateY: [0, 15],
+        scale: [1, 1.1, "easeOutCubic"],
+        shouldAlwaysCompleteAnimation: true
+    };
 
-  const gradientOverlay: BannerLayer = {
-    opacity: [0, 1, "easeOutCubic"],
-    shouldAlwaysCompleteAnimation: true,
-    expanded: false,
-    children: <div className="gradient absolute inset-0"/>
-  };
+    const gradientOverlay: BannerLayer = {
+        opacity: [0, 1, "easeOutCubic"],
+        shouldAlwaysCompleteAnimation: true,
+        expanded: false,
+        children: <div className="gradient absolute inset-0"/>
+    };
 
-  return (
-    <ParallaxBanner
-      layers={[background, headline, foreground, gradientOverlay]}
-      className="h-screen"
-    />
-  );
+    return (
+        <ParallaxBanner
+        layers={[background, headline, foreground, gradientOverlay]}
+        className="h-screen"
+        />
+    );
 };
 
 export default ParallaxScroll;
