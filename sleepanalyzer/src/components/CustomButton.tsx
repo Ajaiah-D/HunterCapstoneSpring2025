e7b833a6eee1ae964798c4bfe2d22e5c94c6481d
Alicia Yang <akthyang@gmail.com>
2025-04-03 15:12:21 -0400

    removed default className if necessary

--- sleepanalyzer/src/components/CustomButton.tsx
+++ sleepanalyzer/src/components/CustomButton.tsx
@@ -5,13 +5,14 @@ type Props = {
     children: React.ReactNode;
     page?: string;
     customization?: string;
+    noOriginalStyle?: boolean;
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
     disabled?: boolean;
 }
 
-const CustomButton = ({ children, page, customization, onClick, disabled }: Props) => {
+const CustomButton = ({ children, page, customization, noOriginalStyle, onClick, disabled }: Props) => {
     const style = 
-      "rounded-md px-10 py-2 bg-lightcoral hover:bg-transparent hover: border-2 hover: text-white hover: border-lightcoral " + customization;
+      noOriginalStyle ? customization : "rounded-md px-10 py-2 bg-lightcoral hover:bg-transparent hover: border-2 hover: text-white hover: border-lightcoral " + customization;
     const button = onClick ? true : false;
 
     if (!button) {
